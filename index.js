ddd261ea972d47953fef577862bb5951320cebda
Brenda <brendakungu04@gmail.com>
2024-02-12 01:21:46 +0300

    JS numbers

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3=12;
+let num4=3;
+let mod = num3/num4 //+ 4;
+
+let set = (20, 1, 3, 4, 5,20)
+let max = Math.max(set);
